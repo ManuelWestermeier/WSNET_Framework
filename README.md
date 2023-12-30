f2a8c69337409e193999cca680829d8801d3b6b3
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2023-12-30 14:43:18 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# WSNET_Framework
+# MWTCP-
+
+The Next Gen of http
+
+# Basic setup
+
+## Client
+
+``` js
+import Client from "./_client";
+
+var API = new Client({
+    host: "localhost",
+    port: 211,
+})
+
+API.onopen = async () => {
+
+    API.say("hello", "__DATA__")
+
+}
+```
+
+## Server
+
+``` js
+const { createServer } = require("./_server");
+
+createServer({ port: 211 }, async client => {
+
+    client.onSay("hello", data => console.log(data))
+
+})
+```
+
+
+## Methods 
+1. Get / onGet
+2. Say / onSay
+3. streamChunk | endStream | isStreamEnd / onStream
---
+++ _client/index.js
@@ -0,0 +1,205 @@
+const log = console.log;
+const chars = "QWERTZUIOPASDFGHJKLYXCVBNMqtzuioasdfghjkyxcvbnm1234567890"
+
+function randomBytes(l) {
+    var str = ""
+
+    for (let index = 0; index < l; index++) {
+        str += chars[Math.floor(Math.random() * chars.length)]
+    }
+
+    return chars
+}
+
+class Client {
+
+    #rawSocket = false
+
+    #obj = {
+        on: {
+            get: {
+                "API\nISSTREAMEND?": key =>
+                    this.#obj.streamEnd?.[key] ?? true
+            },
+            say: {
+                "API\nOnStreamEnd": key => {
+                    this.#obj.on.streamEnd?.[key]?.();
+                }
+            },
+            stream: {},
+            streamEnd: {},
+        },
+        getPromises: {},
+        streamEnd: {},
+    }
+
+    constructor(url) {
+
+        this.#rawSocket = new WebSocket(url)
+
+        this.#rawSocket.onclose = code => {
+            this.onclose(code)
+        }
+
+        this.#rawSocket.onerror = err => {
+            this.onerror(err)
+        }
+
+        this.#rawSocket.onend = () => {
+            this.onend()
+        }
+
+        this.#rawSocket.onopen = () => {
+            this.onopen()
+        }
+
+        this.#rawSocket.onmessage = chunk => {
+            this.#onmessage(chunk)
+        }
+
+    }
+
+    //noraml listeners
+
+    onSay(key, handler) {
+
+        this.#obj.on.say[key] = handler;
+
+    }
+
+    onGet(key, handler) {
+
+        this.#obj.on.get[key] = handler;
+
+    }
+
+    //streams and stream listener
+
+    onStream(key, chunkHandler, onEndHandler) {
+
+        this.#obj.on.stream[key] = chunkHandler;
+        this.#obj.on.streamEnd[key] = () => {
+            onEndHandler();
+            delete this.#obj.on.streamEnd[key];
+            delete this.#obj.on.stream[key];
+        };
+
+    }
+
+    isStreamEnd = async key =>
+        await this.get("API\nISSTREAMEND?", key) ? true : false
+
+    endStream(key) {
+
+        this.#obj.streamEnd[key] = true;
+        this.say("API\nOnStreamEnd", key);
+
+    }
+
+    streamChunk(key, chunk) {
+
+        this.#send("stream", key, chunk, false);
+        this.#obj.streamEnd[key] = false;
+
+    }
+
+    //Normal Methodas
+
+    say(key, data) {
+
+        this.#send("say", key, data, false);
+
+    }
+
+    get(key, data) {
+
+        var id = randomBytes(8);
+
+        return new Promise((reslove) => {
+
+            this.#obj.getPromises[id] = res => {
+                delete this.#obj.getPromises[id];
+                reslove(res);
+            };
+
+            this.#send("get", key, data, id)
+
+        })
+
+    }
+
+    //Send and Onmessage
+
+    #onmessage({ data }) {
+
+        try {
+
+            var data = JSON.parse(data);
+
+            //onSay
+            if (data?.method == "say" && data?.key) {
+                if (this.#obj.on.say?.[data.key])
+                    this.#obj.on.say[data.key](data?.cont)
+            }
+            //onGet
+            else if (data?.method == "get" && data?.key && data?.id) {
+
+                const sendBack = cont => {
+                    this.#send("getback", false, cont, data.id)
+                }
+
+                if (!this.#obj.on.get[data?.key])
+                    return sendBack("404")
+
+                var res = this.#obj.on.get[data?.key](data?.cont)
+
+                if (res instanceof Promise) {
+                    res.then(res => sendBack(res))
+                } else {
+                    sendBack(res)
+                }
+
+            }
+            //For getting data Back
+            else if (data?.method == "getback" && data?.id) {
+                if (this.#obj.getPromises[data.id])
+                    this.#obj.getPromises[data.id](data?.cont);
+            }
+            //onStream
+            else if (data?.method == "stream" && data?.key) {
+                if (this.#obj.on.stream?.[data?.key])
+                    this.#obj.on.stream[data?.key](data?.cont)
+            }
+            //on ?
+            else log(data)
+
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
+
+    #send(method, key, cont, id) {
+
+        var data = {
+            method,
+            cont
+        }
+
+        if (key) data.key = key
+        if (id) data.id = id;
+
+        this.#rawSocket.send(JSON.stringify(data))
+
+    }
+
+    //Other Handlers
+
+    onclose() { }
+    onopen() { }
+    onerror() { }
+    onend() { }
+
+}
+
+export default Client;
